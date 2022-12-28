a5a52c0bb704d2e8ca41be53ada4cd529bfd5b66
danielrubens <danielrubensdaniel@gmail.com>
2022-12-28 14:44:59 -0300

    migrations/orderportions

--- server/src/migrations/20221228163405-orderportions.js
+++ server/src/migrations/20221228163405-orderportions.js
@@ -2,20 +2,19 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('orderportions', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nDup: Sequelize.STRING,
+      dVenc: Sequelize.STRING,
+      vDup: Sequelize.STRING,
+      availableToMarket: Sequelize.INTEGER,
+    })
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface) => { await queryInterface.dropTable('orderportions') }
 };
