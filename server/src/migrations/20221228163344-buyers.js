72d9558656f88760d29ccb21d42407a3cf2651b3
danielrubens <danielrubensdaniel@gmail.com>
2022-12-28 14:36:51 -0300

    migrations/buyers

--- server/src/migrations/20221228163344-buyers.js
+++ server/src/migrations/20221228163344-buyers.js
@@ -2,20 +2,43 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('providers', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: Sequelize.STRING,
+      tradingName: Sequelize.STRING,
+      cashForceTax: Sequelize.STRING,
+      responsibleName: Sequelize.STRING,
+      responsibleEmail: Sequelize.STRING,
+      responsiblePosition: Sequelize.STRING,
+      responsiblePhone: Sequelize.STRING,
+      responsibleMobile: Sequelize.STRING,
+      website: Sequelize.STRING,
+      postalCode: Sequelize.STRING,
+      address: Sequelize.STRING,
+      number: Sequelize.STRING,
+      complement: Sequelize.STRING,
+      neighborhood: Sequelize.STRING,
+      city: Sequelize.STRING,
+      state: Sequelize.STRING,
+      bank: Sequelize.STRING,
+      bankAgency: Sequelize.STRING,
+      account: Sequelize.STRING,
+      documents: Sequelize.STRING,
+      phoneNumber: Sequelize.STRING,
+      situation: Sequelize.STRING,
+      situationDate: Sequelize.STRING,
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+      cnpjId: Sequelize.INTEGER,
+      confirm: Sequelize.INTEGER,
+      email: Sequelize.STRING
+    })
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface) => { await queryInterface.dropTable('providers') }
 };
