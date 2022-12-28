f91906f5c6f0c6f0c5897745721c3d76274c0407
danielrubens <danielrubensdaniel@gmail.com>
2022-12-28 13:40:06 -0300

    app.js

--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,8 @@ const app = require('./app');
 
 const port = process.env.API_PORT || 3000;
 
-app.get('/', (_request, response) => {
-  response.send();
+app.get('/', (_, res) => {
+  res.status(200).json({message: "Hello"});
 });
 
 app.listen(port, () => console.log('ouvindo porta', port));
