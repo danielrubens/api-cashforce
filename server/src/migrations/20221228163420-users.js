5046d7d3dfe78f9c732e4fa611a721fc7834392e
danielrubens <danielrubensdaniel@gmail.com>
2022-12-28 14:49:16 -0300

    migrations/users

--- server/src/migrations/20221228163420-users.js
+++ server/src/migrations/20221228163420-users.js
@@ -2,20 +2,22 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      phoneNumber: Sequelize.STRING,
+      mobile: Sequelize.STRING,
+      departament: Sequelize.STRING,
+      verificationCode: Sequelize.STRING,
+      emailChecked: Sequelize.INTEGER
+    })
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface) => { await queryInterface.dropTable('users') }
 };
