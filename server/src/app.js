bfb28642d26050f274f52730418d6bf01f5f45bd
danielrubens <danielrubensdaniel@gmail.com>
2022-12-31 19:14:03 -0300

    app.get(/) com resposta completa da api

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,12 @@ const controller = require('./controllers')
 const app = express();
 app.use(express.json());
 
+app.get('/', controller.table.getAll)
 app.get('/cnpjs', controller.cnpjs.getAll)
 app.get('/providers', controller.providers.getAll)
 app.get('/users', controller.users.getAll)
 app.get('/buyers', controller.buyers.getAll)
 app.get('/orders', controller.orders.getAll)
-app.get('/table', controller.table.getAll)
 
 
 module.exports = app;
