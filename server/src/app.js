5007a8dc2a2e0a2ee0862a735b93ee8389dbf4ce
danielrubens <danielrubensdaniel@gmail.com>
2023-01-01 22:57:06 -0300

    conectado front com back

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const controller = require('./controllers')
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.get('/', controller.table.getAll)
