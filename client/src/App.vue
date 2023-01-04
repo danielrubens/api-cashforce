aa5259fade558089d56c07bf2a5f03a9b9dd5391
danielrubens <danielrubensdaniel@gmail.com>
2023-01-04 14:11:34 -0300

    notas fiscais aside

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Logo></Logo>
+    <Invoice></Invoice>
     <div class="frame">
       <router-view></router-view>
     </div>
@@ -9,22 +10,28 @@
 
 <script>
 import Logo from './components/Logo'
+import Invoice from './components/Invoice'
 
 export default {
   name: 'App',
   components: {
-    Logo
+    Logo,
+    Invoice
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    display:grid;
+    grid-template-areas:
+            "logo header"
+            "aside content";
+    grid-template-columns: 288px 1fr;
+    grid-template-rows: 80px 1fr;
+  }
+  .frame {
+    grid-area: content;
+  }
+
 </style>
---
+++ client/src/components/Invoice.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="aside side-shadow">
+    <div class="aside-btn" @onLoad="onLoad">
+    <img src="../assets/invoice.svg" class="icon"/>
+      <div class="label">Notas Fiscais<slot></slot></div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Aside',
+  methods: {
+    onLoad () {
+      this.$router.push({name: this.page})
+    }
+  },
+  mounted () {
+    this.$router.push({name: this.page})
+  }
+}
+</script>
+
+<style>
+.aside {
+        grid-area: aside;
+        display:flex;
+        align-items: flex-start;
+        justify-content: flex-start;
+        background-color: #FBFDFE;
+        flex-direction: column;
+    }
+
+    .aside-btn {
+        width: 288px;
+        height: 64px;
+        display: flex;
+        background-color:transparent;
+        flex-direction: row;
+        padding-left: 21px;
+        border-left:transparent solid 4px;
+        transition:200ms;
+        cursor:pointer;
+    }
+
+    .icon {
+        width: 24px;
+    }
+
+    .label {
+      display:flex;
+      align-items: center;
+      justify-content: flex-start;
+      padding-left: 15px;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 18px;
+      color:#00AD8C;
+    }
+
+</style>
